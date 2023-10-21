943d3c4c40fe946ff6383deb094e343d8e06cb9e
hackpirodev <info@hackpiro.dev>
2023-10-21 21:37:20 +0200

    fix: perf on firefox, improved replacing svg with png

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <SvgBackground />
+  <NuxtImg
+    class="absolute top-0 left-0 w-full h-full object-cover -z-10"
+    sizes="100vw sm:50vw md:400px"
+    src="/img/bg.png"
+    alt="background"
+    format="webp"
+  />
   <div
-    class="flex flex-col w-full h-full items-center justify-center scroll-smooth z-100 mt-5 lg:mt-0 gap-5 lg:gap-10 scroll-smooth"
+    class="flex flex-col w-full h-full items-center justify-center scroll-smooth mt-5 lg:mt-0 gap-5 lg:gap-10 scroll-smooth"
   >
     <Socials />
     <SectionHome />
@@ -13,6 +19,20 @@
 </template>
 
 <style>
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px rgb(51, 51, 51);
+  border-radius: 5px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(34, 34, 34);
+  border-radius: 5px;
+}
+
 .name {
   flex-shrink: 0;
   color: #fff;
