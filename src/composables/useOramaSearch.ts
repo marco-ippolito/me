a64cba5073870e08a9e1efdd42e7c5d0732999bb
hackpirodev <info@hackpiro.dev>
2023-10-22 15:01:48 +0200

    feat: biome lints

--- src/composables/useOramaSearch.ts
+++ src/composables/useOramaSearch.ts
@@ -3,31 +3,31 @@ import { computedAsync } from "@vueuse/core";
 import { OramaSchema } from "./types";
 
 export async function useOramaSearch<T extends Record<string, unknown>>({
-  data,
-  schema,
+	data,
+	schema,
 }: {
-  data: T[];
-  schema: OramaSchema<T>;
+	data: T[];
+	schema: OramaSchema<T>;
 }) {
-  const searchTerm = ref("");
-  const db = await create({
-    schema,
-  });
+	const searchTerm = ref("");
+	const db = await create({
+		schema,
+	});
 
-  for (const item of data) {
-    // @ts-ignore
-    await insert(db, item);
-  }
+	for (const item of data) {
+		// @ts-ignore
+		await insert(db, item);
+	}
 
-  const searchResults = computedAsync(async () => {
-    const results = search(db, {
-      term: searchTerm.value,
-    });
-    return results;
-  });
+	const searchResults = computedAsync(async () => {
+		const results = search(db, {
+			term: searchTerm.value,
+		});
+		return results;
+	});
 
-  return {
-    searchTerm,
-    searchResults,
-  };
+	return {
+		searchTerm,
+		searchResults,
+	};
 }
